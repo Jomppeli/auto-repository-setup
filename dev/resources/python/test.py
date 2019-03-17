45080d23a1cacf9c651bf7c450c51ad778003d07
Joonas Mankinen <k2438@student.jamk.fi>
2019-03-17 20:09:33 +0200

    Testing testing

--- dev/resources/python/test.py
+++ dev/resources/python/test.py
@@ -1,16 +1,25 @@
 import json
 import requests
 
-url = "https://api.github.com/jomppeli/repos"
-data = {"name": "Hello-World",
-  "description": "This is your first repository",
-  "homepage": "https://github.com",
-  "private": false,
-  "has_issues": true,
-  "has_projects": true,
-  "has_wiki": true}
+#url = "https://api.github.com/users/jomppeli/repos"
+#url = "https://api.github.com/repos/jomppeli/cheatsheet/readme"
 
-r = requests.post(url=url, data=data)
+url = "https://api.github.com/repos/jomppeli/cheatsheet/contents/test/test"
+headers = {"Authorization":"token f6e77b894866e4139fab5b8bb9d4c52db27ec968"}
 
-response = r.json
+data = {
+  "message": "my commit message",
+  "committer": {
+    "name": "Scott Chacon",
+    "email": "schacon@gmail.com"
+  },
+  "content": "bXkgbmV3IGZpbGUgY29udGVudHM="
+}
+
+#r = requests.post(url=url, data=data)
+#r = requests.get(url=url,headers=headers)
+r = requests.put(url=url,headers=headers,data=data)
+
+
+response = r.json()
 print(response)
