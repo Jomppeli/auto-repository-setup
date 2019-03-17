74337af9bcf1c7a4137818e8db373ad1053f22a2
Joonas Mankinen <k2438@student.jamk.fi>
2019-03-17 23:06:53 +0200

    testing

--- dev/resources/python/test1.py
+++ dev/resources/python/test1.py
@@ -11,7 +11,7 @@ data = open('data.txt')
 
 filename = input("Filename: ")
 username = input("Username: ")
-password = getpass.getpass("Password:")
+password = getpass.getpass("Password: ")
 response = requests.put('https://api.github.com/repos/jomppeli/cheatsheet/contents/docs/%s' % filename, 
 data, auth=(username, password))
 print(response)
